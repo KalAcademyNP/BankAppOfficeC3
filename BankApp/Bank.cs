c6784a44265f4f274776d8fa48d6d1ffb3159da5
Kal Viswanathan <Kalpanav@microsoft.com>
2019-06-24 13:03:42 -0700

    Accounts and transactions, LINQ and Lambda

--- BankApp/Bank.cs
+++ BankApp/Bank.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace BankApp
@@ -8,6 +9,7 @@ namespace BankApp
     static class Bank
     {
         private static readonly List<Account> accounts = new List<Account>();
+        private static readonly List<Transaction> transactions = new List<Transaction>();
 
         public static Account CreateAccount(string emailAddress, string accountName, 
             AccountTypes accountType = AccountTypes.Checking, decimal initialDeposit = 0)
@@ -28,7 +30,60 @@ namespace BankApp
 
         public static IEnumerable<Account> GetAllAccountsByEmailAddress(string emailAddress)
         {
+            return accounts.Where(a => a.EmailAddress == emailAddress);
+        }
 
+        public static void Deposit(int accountNumber, decimal amount)
+        {
+            var account = GetAccountByAccountNumber(accountNumber);
+            account.Deposit(amount);
+
+            var transaction = new Transaction
+            {
+                TransactionDate = DateTime.Now,
+                Description = "Bank deposit",
+                Amount = amount,
+                AccountNumber = accountNumber,
+                TransactionType = TypeOfTransaction.Credit
+            };
+
+            transactions.Add(transaction);
+        }
+
+        public static void Withdraw(int accountNumber, decimal amount)
+        {
+            var account = GetAccountByAccountNumber(accountNumber);
+            account.Withdraw(amount);
+            var transaction = new Transaction
+            {
+                TransactionDate = DateTime.Now,
+                Description = "Bank Withdrawal",
+                Amount = amount,
+                AccountNumber = accountNumber,
+                TransactionType = TypeOfTransaction.Debit
+            };
+
+            transactions.Add(transaction);
+
+        }
+
+        public static Account GetAccountByAccountNumber(int accountNumber)
+        {
+            var account = accounts.SingleOrDefault(a => a.AccountNumber == accountNumber);
+            if (account == null)
+            {
+                return null;
+            }
+
+            return account;
+        }
+
+
+        public static IEnumerable<Transaction> GetTransactionsByAccountNumber(int accountNumber)
+        {
+            return transactions
+                    .Where(t => t.AccountNumber == accountNumber)
+                    .OrderByDescending(t => t.TransactionDate);
         }
     }
 }
--- BankApp/Program.cs
+++ BankApp/Program.cs
@@ -16,6 +16,7 @@ namespace BankApp
                 Console.WriteLine("2. Deposit");
                 Console.WriteLine("3. Withdraw");
                 Console.WriteLine("4. Print all accounts");
+                Console.WriteLine("5. Print all transactions");
 
                 Console.Write("Select an option: ");
                 var choice = Console.ReadLine();
@@ -49,10 +50,38 @@ namespace BankApp
 
                         break;
                     case "2":
+                        PrintAllAccounts();
+                        Console.Write("Account number: ");
+                        var accountNumber = Convert.ToInt32(Console.ReadLine());
+                        Console.Write("Amount to deposit: ");
+                        var amount = Convert.ToDecimal(Console.ReadLine());
+                        Bank.Deposit(accountNumber, amount);
+                        Console.WriteLine("Deposit successful!");
                         break;
                     case "3":
+                        PrintAllAccounts();
+                        Console.Write("Account number: ");
+                        accountNumber = Convert.ToInt32(Console.ReadLine());
+                        Console.Write("Amount to withdraw: ");
+                        amount = Convert.ToDecimal(Console.ReadLine());
+                        Bank.Withdraw(accountNumber, amount);
+                        Console.WriteLine("Withdrawal successful!");
                         break;
                     case "4":
+                        PrintAllAccounts();
+
+                        break;
+                    case "5":
+                        PrintAllAccounts();
+                        Console.Write("Account number: ");
+                        accountNumber = Convert.ToInt32(Console.ReadLine());
+                        var transactions = Bank.GetTransactionsByAccountNumber(accountNumber);
+                        foreach (var transaction in transactions)
+                        {
+                            Console.WriteLine($"TT: {transaction.TransactionType}, " +
+                                $"TD: {transaction.TransactionDate}, " +
+                                $"Amount: {transaction.Amount:C}");
+                        }
                         break;
                     default:
                         break;
@@ -60,6 +89,21 @@ namespace BankApp
                 }
             }
 
+        }
+
+        private static void PrintAllAccounts()
+        {
+            Console.WriteLine("Email Address: ");
+            var emailAddress = Console.ReadLine();
+            var accounts = Bank.GetAllAccountsByEmailAddress(emailAddress);
+            foreach (var account in accounts)
+            {
+                Console.WriteLine($"Account Number: {account.AccountNumber}, " +
+                    $"AName: {account.AccountName}, " +
+                    $"AType: {account.AccountType}, " +
+                    $"Email: {account.EmailAddress}, " +
+                    $"Balance: {account.Balance:C}");
+            }
         }
     }
 }
